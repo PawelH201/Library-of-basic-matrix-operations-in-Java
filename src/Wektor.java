2d71d8eb7f3906ab5485ca9af094dba82e67b4e0
Paweł Tymoteusz Hulanicki <p.hulanicki@stud.elka.pw.edu.pl>
2020-05-07 17:32:25 +0200

    Poprawki kosmetyczne wektor i iloczynwektorowy

--- src/Wektor.java
+++ src/Wektor.java
@@ -2,47 +2,51 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.lang.IllegalArgumentException;
 
+/**
+ * @author phulanic
+ */
 
 public class Wektor
 {
-    public double[] wektor; //deklaracja prywatnej tablicy jednowymiarowej wektor
-
-    public double[] getWektor() // zwraca tablicę wektor
+    //deklaracja tablicy zmiennych typu double o nazwie wektor (nazwa klasy to Wektor - wielka litera)
+    public double[] wektor;
+// metoda get dla klasy Wektor
+    public double[] getWektor()
     {
         return wektor;
     }
-
-    public void setWektor(double... wektor) // ustawia wartość tablicy wektor
+// setter dla klasy Wektor
+    public void setWektor(double... wektor)
     {
         this.wektor = wektor;
     }
-
-    public Wektor(double... wektor) // konstuktor obiektu Wektor
+//konstruktor klasy
+    public Wektor(double... wektor)
     {
         this.wektor = wektor;
     }
-
-    static Wektor wektor(double... wektorparametr) // deklaracja metody Wektor(), która zwraca tablicę w typie danych double
+//deklaracja metody wektor, tworzy tablice ze zmiennymi double a nastepnie ja zwraca
+    static Wektor wektor(double... wektorparametr)
     {
         Wektor wektor = new Wektor(wektorparametr);
-        return wektor; // zwraca parametry w obiekcie Wektor
+        return wektor;
     }
-
+//metoda toString dla klasy Wektor
     @Override
     public String toString() {
         return "Wektor{" +
                 "wektor=" + Arrays.toString(wektor) +
                 '}';
     }
-
+//czesc testowa
     public static void main(String[] args)
     {
-       // Wektor wektor1 = new Wektor(2,3,4,5,6);
-      //  System.out.println(wektor1.wektor);
-        //System.out.println(wektor1.toString());
-        //System.out.println(wektor1.getWektor());
+        Wektor wektor1 = new Wektor(2,3,4,5,6);
+        System.out.println(wektor1.wektor);
+        System.out.println(wektor1.toString());
+        System.out.println(wektor1.getWektor());
 
 
-        //System.out.println(wektor1);
+        System.out.println(wektor1);
     }
 }
--- src/iloczynwektorowy.java
+++ src/iloczynwektorowy.java
@@ -1,37 +1,50 @@
+/**
+ * @author phulanic
+ */
+
+
 class iloczynwektorowy extends Wektor{
+    //deklaracja zmiennych typu wektor
     boolean czymoznapoliczyciloczynwektorowy;
     Wektor wektorpierwszy;
     Wektor wektordrugi;
     Wektor wynik;
+    //deklaracja metody iloczynwektorowy, obliczająca iloczyn dla wektorów w R3
     static Wektor iloczynwektorowy(Wektor pierwszy, Wektor drugi) {
-       double[] wektorpierwszy = pierwszy.getWektor();
+       //wyciągniecie tablic z obiektow Wektor
+        double[] wektorpierwszy = pierwszy.getWektor();
        double[] wektordrugi = drugi.getWektor();
+       //wektor w ktorym bedzie pojawial sie wynik
        Wektor wynik;
-
+//warunek sprawdzajacy czy wymiary wektorow sie zgadzaja
         if(wektordrugi.length == 3 && wektorpierwszy.length == 3){
+            // przypisanie kolejnych wartosci dla zmiennych w celu poprawienia czytelnosci kodu
             double a0 = wektorpierwszy[0];
             double a1 = wektorpierwszy[1];
             double a2 = wektorpierwszy[2];
             double b0 = wektordrugi[0];
             double b1 = wektordrugi[1];
             double b2 = wektordrugi[2];
+            //deklaracja zmiennych bedacych czescia wyniku
             double w0;
             double w1;
             double w2;
+            //obliczenie wartosci wyniku
             w0 = a1*b2 - a2*b1;
             w1 = a2*b0 - a0*b2;
             w2 = a0*b1 - a1*b0;
+            //zapis trzech zmiennych typu double do obiektu typu wektor, zwrocenie wyniku
             wynik = new Wektor(w0,w1,w2);
             return wynik;
         }
+        //przypadek gdy wymiar podanych wektorow jest niepoprawny
         else {
-            wynik = null;
-            return wynik;
+            throw new IllegalArgumentException("Sprawdz czy podane argumenty sa poprawne");
         }
 
        }
 
-
+//czesc testowa programu
 
    public static void main(String args[]){
         Wektor pierwszy = new Wektor(1,2,4);
