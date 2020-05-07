3d38b903c9a64bfa6a7a642b9a4b2d570fa4c1e4
Paweł Tymoteusz Hulanicki <p.hulanicki@stud.elka.pw.edu.pl>
2020-05-07 16:12:31 +0200

    wektor 0.2 + szkielet iloczynwektorowy

--- src/Wektor.java
+++ src/Wektor.java
@@ -17,25 +17,25 @@ public class Wektor
         this.wektor = wektor;
     }
 
-    public Wektor(double... wektor) // konstrkutkor obiektu Wektor
+    public Wektor(double... wektor) // konstuktor obiektu Wektor
     {
         this.wektor = wektor;
+        this.
     }
 
-    static Wektor wektor(double... wektorparametr) // deklaracja metody macierz(), która zwraca tablicę tablic w typie danych double
+    static Wektor wektor(double... wektorparametr) // deklaracja metody Wektor(), która zwraca tablicę w typie danych double
     {
         Wektor wektor = new Wektor(wektorparametr);
-        return wektor; // zwraca parametry macierzy w obiekcie Macierz
+        return wektor; // zwraca parametry w obiekcie Wektor
     }
 
 
     public static void main(String[] args)
     {
         Wektor macierz1 = new Wektor(2,3,4,5,6);
+        System.out.println(macierz1.wektor[0]);
 
-        //System.out.println(macierz(new double[]{3,4.8,3.3432}, new double[]{2,4.23,-2321.32,0.43})); // wyrzuca wyjątek
 
         System.out.println(macierz1);
-        //System.out.println(macierz2); // wyrzuca wyjątek
     }
 }
---
+++ src/iloczynwektorowy.java
@@ -0,0 +1,20 @@
+class iloczynwektorowy extends Wektor{
+    boolean czymoznapoliczyciloczynwektorowy;
+    Wektor wektorpierwszy;
+    Wektor wektordrugi;
+    public iloczynwektorowy(Wektor pierwszy, Wektor drugi) {
+        this.czymoznapoliczyciloczynwektorowy = false;
+        this.wektorpierwszy = pierwszy;
+        this.wektordrugi = drugi;
+    }
+
+
+
+
+    public static void main(String args[]){
+
+    }
+
+
+
+}
