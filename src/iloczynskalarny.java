4f39dd2caaaca7b9945f57b347d8d8f0ef4c4fc4
Paweł Tymoteusz Hulanicki <p.hulanicki@stud.elka.pw.edu.pl>
2020-05-14 19:54:43 +0200

    Iloczyn skalarny 0.2

--- src/iloczynskalarny.java
+++ src/iloczynskalarny.java
@@ -15,19 +15,26 @@ public class iloczynskalarny extends Wektor {
         //wektor w ktorym bedzie pojawial sie wynik
         Wektor wynik;
         if (dlugosc1 == dlugosc2) {
+
+            double[] listawynik = new double[dlugosc1];
+            for (int indeks = 0; indeks < dlugosc1; indeks++) {
+                double pierwszawartosc;
+                double drugawartosc;
+                double wartoscwynik;
+                pierwszawartosc = wektorpierwszy[indeks];
+                drugawartosc = wektordrugi[indeks];
+                wartoscwynik = pierwszawartosc * drugawartosc;
+                listawynik[indeks] = wartoscwynik;
+
+
+            }
+            Wektor rezultat = new Wektor(listawynik);
+            return rezultat;
+
         }
-        double[] listawynik = new double[dlugosc1];
-        for (int indeks = 0; indeks < dlugosc1; indeks++) {
-            double pierwszawartosc;
-            double drugawartosc;
-            double wartoscwynik;
-            pierwszawartosc = wektorpierwszy[indeks];
-            drugawartosc = wektordrugi[indeks];
-            wartoscwynik = pierwszawartosc * drugawartosc;
-            listawynik[indeks] = wartoscwynik;
+        else {
+            throw new IllegalArgumentException("Sprawdz czy wektory mają taką samą długość i mają odpowiednie dane!");
         }
-        Wektor rezultat = new Wektor(listawynik);
-        return rezultat;
 
 
     }
@@ -35,7 +42,7 @@ public class iloczynskalarny extends Wektor {
 
     public static void main(String args[]) {
         //utworzenie obiektow wektor
-        Wektor pierwszy = new Wektor(1.2, 2, 4,5,4);
+        Wektor pierwszy = new Wektor(2, 2, 4,5,4);
         Wektor drugi = new Wektor(1, 2, 3,4,3);
         System.out.println(iloczynskalarny(pierwszy, drugi));
     }
