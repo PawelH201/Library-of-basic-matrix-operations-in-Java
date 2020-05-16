0e6d2085b43d4ff2344471fc769c3bb0dc2f3cb6
Jowita Wiktoria Walczuk <j.walczuk@stud.elka.pw.edu.pl>
2020-05-17 01:56:11 +0200

    Klasa zawierająca metody sumowania i odejmowania macierzy

--- src/SumaiRóżnicaMacierzy.java
+++ src/SumaiRóżnicaMacierzy.java
@@ -1,43 +1,51 @@
 //Klasa potomna klasy Macierz zawierająca metody sumaMacierzy, różnica Macierzy.
-//ALERT ->>> Program jest w trakcie tworzenia(nie funkcjonuje jeszcze sprawnie)
-public class SumaiRóżnicaMacierzy extends Macierz {/*
 
-    double[][] wynik=null;
-    double[][] tablica1=getMacierz();
-    double[][] tablica2=getMacierz();
+public class SumaiRóżnicaMacierzy extends Macierz {
 
-    public void sumaMacierzy(Macierz macierz) { //metoda służąca dodawaniu macierzy
+    public static Macierz sumaMacierzy(Macierz macierz, Macierz macierz2) { //metoda służąca dodawaniu macierzy
+
+        double[][] tablica1=macierz.getMacierz();
+        double[][] tablica2=macierz2.getMacierz();
+        double[][] wynik = new double[tablica2.length][tablica2[0].length];
 
         if (tablica1.length == tablica2.length && tablica2[0].length == tablica1[0].length) { //czy macierze mają takie same wymiary? -> czy operacja dodawania jest możliwa?
-            tablica2 = new double[tablica2.length][tablica2[0].length];
-            for (int i = 0; i < tablica2.length; i++) {
-                for (int j = 0; j < tablica2[0].length; j++) {
-                    wynik[i][j] = (tablica2[i][j] + tablica1[i][j]);
+            for (int i = 0; i < tablica1.length; i++) {                                        //iterowanie po wierszach i kolumnach macierzy
+                for (int j = 0; j < tablica1[0].length; j++) {
+                        wynik[i][j]=(tablica2[i][j] + tablica1[i][j]);
+                    }
                 }
             }
-        } else {
+        else {
             throw new RuntimeException("Operacja dodawania nie jest możliwa dla macierzy o różnych wymiarach!!! ");
         }
-
+        Macierz Wynik = new Macierz(wynik);
+        return Wynik;
     }
 
-    public void różnicaMacierzy(Macierz macierz) {
+    public static Macierz różnicaMacierzy(Macierz macierz,Macierz macierz2) {                  //metoda służąca odejmowaniu macierzy
 
+        double[][] tablica1=macierz.getMacierz();
+        double[][] tablica2=macierz2.getMacierz();
+        double[][] wynik=new double[tablica1.length][tablica2[0].length];
         if (tablica1.length == tablica2.length && tablica2[0].length == tablica1[0].length) {//czy macierze mają takie same wymiary? -> czy operacja odejmowania jest możliwa
-            tablica2 = new double[tablica2.length][tablica2[0].length];
-            for (int i = 0; i < tablica2.length; i++) {
+            for (int i = 0; i < tablica2.length; i++) {                                      //iterowanie po wierszach i kolumnach macierzy
                 for (int j = 0; j < tablica2[0].length; j++) {
-                    wynik[i][j] = (tablica2[i][j] - tablica1[i][j]);
+                    wynik[i][j] = (tablica1[i][j] - tablica2[i][j]);
                 }
             }
         } else {
             throw new RuntimeException("Operacja odejmowania nie jest możliwa dla macierzy o różnych wymiarach!!! ");
         }
+        Macierz Suma=new Macierz(wynik);
+        return Suma;
     }
 
-    public static void main(String[] args) { //program testowy
-        Macierz macierz1 = new Macierz(new double[]{23,34.2,23.2}, new double[]{2,3,4});
-        Macierz macierz2 = new Macierz(new double[]{23,34.2,23.2}, new double[]{2,34});
-        System.out.println(macierz1.sumaMacierzy( macierz2)); //jak widać prace jeszcze trwają
-    }*/
+    public static void main(String[] args) { //kod testowy
+        Macierz macierz1 = new Macierz(new double[]{1.0,10.0,100.0}, new double[]{2.02,20.02,200.02});
+        Macierz macierz2 = new Macierz(new double[]{3.0,30.0,300.0}, new double[]{4.0,40.0,400.0});
+        System.out.println(macierz1);
+        System.out.println(macierz2);
+        System.out.println("Suma macierzy:  " + sumaMacierzy(macierz1,macierz2));
+        System.out.println("Różnica macierzy:  " + różnicaMacierzy(macierz1,macierz2));
+    }
 }
