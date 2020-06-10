abf8d1da6125318010544c17420e038c2909e807
Mikołaj Piotr Pater <m.pater@stud.elka.pw.edu.pl>
2020-06-10 17:06:15 +0200

    Poprawiono kalsę RownaniaCramer oraz przeniesiono je do pakietu MacierzeIWektory

---
+++ src/MacierzeiWektory/RownaniaCramer.java
@@ -0,0 +1,58 @@
+package MacierzeiWektory;
+
+public class RownaniaCramer extends Wyznacznik {
+
+    static double[] ukladRownan(Macierz macierz) {
+
+        int wierszeMacierz = macierz.getMacierz().length;
+        int kolumnyMacierz = macierz.getMacierz()[0].length;
+
+        double[][] wartoscimacierzy = macierz.getMacierz();
+        double[] wyrazywolne = new double[wierszeMacierz];
+        double[][] macierzpodstawowa = new double[wierszeMacierz][wierszeMacierz];
+        double[] wynik = new double[wierszeMacierz];
+        double[][] macierzZDopelnieniem = new double[wierszeMacierz][wierszeMacierz];
+
+        if(kolumnyMacierz - wierszeMacierz == 1) {
+            for(int i = 0; i < wierszeMacierz; i++){
+                wyrazywolne[i] = wartoscimacierzy[i][kolumnyMacierz - 1];
+                for(int j = 0; j<kolumnyMacierz-1;j++){
+                    macierzpodstawowa[i][j] = wartoscimacierzy[i][j];
+                }
+            }
+            Macierz MacierzPodstawowa = new Macierz(macierzpodstawowa);
+            for(int niewiadoma = 0; niewiadoma < wierszeMacierz; niewiadoma++){
+                for (int wiersze = 0; wiersze < wierszeMacierz; wiersze++){
+                    for(int kolumny = 0; kolumny < kolumnyMacierz-1; kolumny++)
+                        macierzZDopelnieniem[wiersze][kolumny] = macierzpodstawowa[wiersze][kolumny];
+                }
+                for(int a = 0; a < wierszeMacierz; a++){
+                    macierzZDopelnieniem[a][niewiadoma] = wyrazywolne[a];
+                }
+                Macierz MacierzZDopelnieniem = new Macierz(macierzZDopelnieniem);
+                if (wyznacznikMacierzy(MacierzPodstawowa) == 0)
+                {
+                    throw new IllegalArgumentException("Podana macierz podstawowa ma wyznacznik zerowy, nie można określić oznaczonego rozwiązania tego układu równań");
+                }
+                wynik[niewiadoma] = wyznacznikMacierzy(MacierzZDopelnieniem) / wyznacznikMacierzy(MacierzPodstawowa);
+            }
+            for (int i = 0; i < wynik.length; i++)
+            {
+                double tmp = wynik[i];
+                wynik[i] = Math.round((tmp) * 1000000000.0)/1000000000.0; //zaokrąglenie do 6 miejsc po przecinku (ilość zer)
+            }
+            return wynik;
+        }
+        else {
+            throw new IllegalArgumentException("Podana macierz nie ma jednoznacznego rozwiązania! Sprawdź wymiary macierzy");
+        }
+    }
+
+    /*
+    public static void main(String[] args){
+        Macierz macierz1 = macierz(new double[]{0,2,0,88}, new double[]{0,3,1,5}, new double[]{3,0,0,3});
+        System.out.println(Arrays.toString(ukladRownan(macierz1)));
+    }
+
+     */
+}
