c7797e182350f0f6dd62f940f04d6d0fc9f11872
Mikołaj Piotr Pater <m.pater@stud.elka.pw.edu.pl>
2020-06-10 22:11:29 +0200

    Zauktualizowano klasę Kalkulator

--- src/Kalkulator.java
+++ src/Kalkulator.java
@@ -1,4 +1,6 @@
 import MacierzeiWektory.*;
+
+import java.awt.geom.Area;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -38,7 +40,7 @@ public class Kalkulator
 
             if (indeks == 1)
             {
-                System.out.println("Wybrano operację sumowania wektorów");
+                System.out.println("Wybrano operację sumowania wektorów \n");
                 Wektor wektor1;
                 Wektor wektor2;
 
@@ -90,7 +92,7 @@ public class Kalkulator
             }
             else if (indeks == 2)
             {
-                System.out.println("Wybrano operację odejmowania wektorów");
+                System.out.println("Wybrano operację odejmowania wektorów \n");
                 Wektor wektor1;
                 Wektor wektor2;
 
@@ -150,7 +152,7 @@ public class Kalkulator
             }
             else if (indeks == 5)
             {
-                System.out.println("Wybrano operację sumowania macierzy");
+                System.out.println("Wybrano operację sumowania macierzy \n");
 
                 Macierz macierz1;
                 Macierz macierz2;
@@ -244,7 +246,97 @@ public class Kalkulator
             }
             else if (indeks == 6)
             {
+                System.out.println("Wybrano operację odejmowania macierzy \n");
 
+                Macierz macierz1;
+                Macierz macierz2;
+
+                System.out.println("Podaj liczbę wierszy pierwszej macierzy");
+                int wiersze1 = scanner.nextInt();
+                System.out.println("Podaj liczbę kolumn pierwszej macierzy");
+                int kolumny1 = scanner.nextInt();
+                System.out.println("Macierz ma następujące rozmiary: " + wiersze1 + "X" + kolumny1);
+
+                double[][] parametr1 = new double[wiersze1][kolumny1];
+
+                for (int i = 0; i < wiersze1; i++)
+                {
+                    for (int n = 0; n < kolumny1; n++)
+                    {
+                        System.out.print("Podaj wartość dla " + (i+1) + ". wiersza, " + (n+1) + ". kolumny: ");
+                        double wartosc1 = scanner.nextDouble();
+                        parametr1[i][n] = wartosc1;
+                    }
+                }
+                try
+                {
+                    macierz1 = Macierz.macierz(parametr1);
+                    System.out.println("Macierz 1. wygląda następująco: ");
+                    for (int i = 0; i < wiersze1; i++)
+                    {
+                        System.out.println(Arrays.toString(macierz1.getMacierz()[i]));
+                    }
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe rozmiary macierzy!");
+                }
+
+                System.out.println("Podaj liczbę wierszy drugiej macierzy");
+                int wiersze2 = scanner.nextInt();
+                System.out.println("Podaj liczbę kolumn drugiej macierzy");
+                int kolumny2 = scanner.nextInt();
+                System.out.println("Macierz ma następujące rozmiary: " + wiersze2 + "X" + kolumny2);
+
+                double[][] parametr2 = new double[wiersze2][kolumny2];
+
+                for (int i = 0; i < wiersze2; i++)
+                {
+                    for (int n = 0; n < kolumny2; n++)
+                    {
+                        System.out.print("Podaj wartość dla " + (n+1) + ". wiersza, " + (n+1) + ". kolumny: ");
+                        double wartosc2 = scanner.nextDouble();
+                        parametr2[i][n] = wartosc2;
+                    }
+                }
+                try
+                {
+                    macierz2 = Macierz.macierz(parametr2);
+                    System.out.println("Macierz 2. wygląda następująco: ");
+                    for (int i = 0; i < wiersze1; i++)
+                    {
+                        System.out.println(Arrays.toString(macierz2.getMacierz()[i]));
+                    }
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe rozmiary macierzy!");
+                }
+
+                try
+                {
+                    macierz1 = Macierz.macierz(parametr1);
+                    macierz2 = Macierz.macierz(parametr2);
+
+                    Macierz roznica = SumaiRoznicaMacierzy.roznicaMacierzy(macierz1, macierz2);
+                    System.out.println("\nRóżnica tych macierzy wygląda następująco: ");
+                    for (int i = 0; i < wiersze1; i++)
+                    {
+                        System.out.println(Arrays.toString(roznica.getMacierz()[i]));
+                    }
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe rozmiary macierzy!");
+                }
+
+                System.out.println("Wciśnij enter, aby kontynuować...");
+                try
+                {
+                    System.in.read();
+                }
+                catch (Exception ignored)
+                {}
             }
             else if (indeks == 7)
             {
@@ -256,15 +348,161 @@ public class Kalkulator
             }
             else if (indeks == 9)
             {
+                System.out.println("Wybrano operację liczenia wyznacznika macierzy \n");
 
+                System.out.println("Podaj liczbę wierszy pierwszej macierzy");
+                int wiersze = scanner.nextInt();
+                System.out.println("Podaj liczbę kolumn pierwszej macierzy");
+                int kolumny = scanner.nextInt();
+                System.out.println("Macierz ma następujące rozmiary: " + wiersze + "X" + kolumny);
+
+                double[][] parametr = new double[wiersze][kolumny];
+                Macierz macierz;
+
+                for (int i = 0; i < wiersze; i++)
+                {
+                    for (int n = 0; n < kolumny; n++)
+                    {
+                        System.out.print("Podaj wartość dla " + (i+1) + ". wiersza, " + (n+1) + ". kolumny: ");
+                        double wartosc = scanner.nextDouble();
+                        parametr[i][n] = wartosc;
+                    }
+                }
+                try
+                {
+                    macierz = Macierz.macierz(parametr);
+
+                    System.out.println("Macierz wygląda następująco: ");
+                    for (int i = 0; i < wiersze; i++)
+                    {
+                        System.out.println(Arrays.toString(macierz.getMacierz()[i]));
+                    }
+
+                    double wyznacznik = Wyznacznik.wyznacznikMacierzy(macierz);
+                    System.out.println("\nJej wyznacznik wynosi: " + wyznacznik);
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe wymiary macierzy - nie jest kwadratowa!");
+                }
+
+                System.out.println("Wciśnij enter, aby kontynuować...");
+                try
+                {
+                    System.in.read();
+                }
+                catch (Exception ignored)
+                {}
             }
             else if (indeks == 10)
             {
+                System.out.println("Wybrano operację liczenia macierzy odwrotnej \n");
 
+                System.out.println("Podaj liczbę wierszy pierwszej macierzy");
+                int wiersze = scanner.nextInt();
+                System.out.println("Podaj liczbę kolumn pierwszej macierzy");
+                int kolumny = scanner.nextInt();
+                System.out.println("Macierz ma następujące rozmiary: " + wiersze + "X" + kolumny);
+
+                double[][] parametr = new double[wiersze][kolumny];
+                Macierz macierz;
+
+                for (int i = 0; i < wiersze; i++)
+                {
+                    for (int n = 0; n < kolumny; n++)
+                    {
+                        System.out.print("Podaj wartość dla " + (i+1) + ". wiersza, " + (n+1) + ". kolumny: ");
+                        double wartosc = scanner.nextDouble();
+                        parametr[i][n] = wartosc;
+                    }
+                }
+                try
+                {
+                    macierz = Macierz.macierz(parametr);
+
+                    System.out.println("Macierz wygląda następująco: ");
+                    for (int i = 0; i < wiersze; i++)
+                    {
+                        System.out.println(Arrays.toString(macierz.getMacierz()[i]));
+                    }
+
+                    Macierz macierzodwrotna = MacierzOdwrotna.macierzOdwrotna(macierz);
+                    System.out.println("Macierz odwrotna wygląda następująco: ");
+                    for (int i = 0; i < wiersze; i++)
+                    {
+                        System.out.println(Arrays.toString(macierzodwrotna.getMacierz()[i]));
+                    }
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe wymiary macierzy - nie jest kwadratowa!");
+                }
+
+                System.out.println("Wciśnij enter, aby kontynuować...");
+                try
+                {
+                    System.in.read();
+                }
+                catch (Exception ignored)
+                {}
             }
             else if (indeks == 11)
             {
+                System.out.println("Wybrano operację liczenia układu liniowego za pomocą metody Cramera  \n");
 
+                System.out.println("Podaj takie rozmiary, żeby macierz była takiej postaci: N X N+1 - ma to być macierz rozszerzona o wyrazy wolne");
+                System.out.println("Podaj liczbę wierszy pierwszej macierzy");
+                int wiersze = scanner.nextInt();
+                System.out.println("Podaj liczbę kolumn pierwszej macierzy");
+                int kolumny = scanner.nextInt();
+                System.out.println("Macierz ma następujące rozmiary: " + wiersze + "X" + kolumny);
+
+                double[][] parametr = new double[wiersze][kolumny];
+                Macierz macierz;
+                double[] wyrazywolne;
+
+                for (int i = 0; i < wiersze; i++)
+                {
+                    for (int n = 0; n < kolumny; n++)
+                    {
+                        System.out.print("Podaj wartość dla " + (i+1) + ". wiersza, " + (n+1) + ". kolumny: ");
+                        double wartosc = scanner.nextDouble();
+                        parametr[i][n] = wartosc;
+                    }
+                }
+                try
+                {
+                    macierz = Macierz.macierz(parametr);
+
+                    System.out.println("Macierz wygląda następująco: ");
+                    for (int i = 0; i < wiersze; i++)
+                    {
+                        System.out.println(Arrays.toString(macierz.getMacierz()[i]));
+                    }
+
+                    wyrazywolne = RownaniaCramer.ukladRownan(macierz);
+                    System.out.println("Rozwiązanie tego układu równań wygląda następująco: ");
+                    for (int i =0; i < wyrazywolne.length; i++)
+                    {
+                        System.out.println("X" + (i+1) + " = " + wyrazywolne[i]);
+                    }
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Podano złe wymiary macierzy!");
+                }
+                catch (ArithmeticException e)
+                {
+                    System.out.println("Podana macierz podstawowa ma wyznacznik 0 - nie jest to układ oznaczony!");
+                }
+
+                System.out.println("Wciśnij enter, aby kontynuować...");
+                try
+                {
+                    System.in.read();
+                }
+                catch (Exception ignored)
+                {}
             }
             else if (indeks == 12)
             {
